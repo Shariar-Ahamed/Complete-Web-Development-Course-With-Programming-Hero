228583392afac4d845e8ef541ca4dd7d5f4950ef
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-21 08:10:52 +0600

    Different ways to use addEventListener on a button

---
+++ Milestone 5 Interactive JavaScript, DOM/Module 25 Event, addEventListener, Event bubble/25-3 Different ways to use addEventListener on a button/node.js
@@ -0,0 +1,49 @@
+// TODO: Optional 1 for event handler
+//* internal js use for one click 
+
+
+
+
+// TODO: Optional 2 for event handler
+
+function makeYellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+
+
+
+//Todo: option 3: get Element by Id and then set onclik
+
+const btnMakeBlue = document.getElementById("btn-make-blue");
+// console.log(btnMakeBlue);
+
+btnMakeBlue.onclick = function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+};
+
+
+
+
+//Todo: different version
+
+const btnMakePurple = document.getElementById('btn-make-purple')
+
+btnMakePurple.onclick = makePurple;
+
+  function makePurple(){
+      document.body.style.backgroundColor = 'purple';
+  }
+
+  
+
+
+//! addEventListener Matod(Recommended)
+//document.getElementbyId().addEventListener('event type', handler)
+document.getElementById('btn-make-green').addEventListener('click', function makeGreen(){
+      document.body.style.backgroundColor = 'green';
+})
